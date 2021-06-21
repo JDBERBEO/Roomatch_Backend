bbd85f3f454561c7979bd4e2680f85a9040f2b2c
kellycora <kellyc@evollve.co>
2021-06-21 11:00:28 -0500

    modelo usuario roomate

--- src/models/UserRM.model.js
+++ src/models/UserRM.model.js
@@ -1,4 +1,5 @@
-const { Schema , model } = require('mongoose')
+const { Schema , model, models } = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
     name: {
@@ -8,20 +9,41 @@ const userSchema = new Schema({
     "last-name": { 
         type: String,
         required: true,
-        minlength: [6, 'el nombre es requerido']
+        minlength: [1, 'last name is required']
     },
-    email: { 
+    email: {
         type: String,
-        required: [true, 'el campo email es requerido'],
-    },
+        required: true,
+        validate: [
+          {
+            async validator(email) {
+             try {
+                const userRm = await models.UserRm.findOne({ email })
+                return !userRm
+              } catch(error) {
+                return false
+              }
+            },
+            message: 'email is required'
+          }
+        ]
+      },
+    password: String,
     age: { 
         type: Number,
         required: true,
     },
     description: String,
+    default: false,
     ranking : [],
 },{
     timestamps: true 
+})
+
+userSchema.pre('save', async function() {
+    if(this.password && this.isModified('password')){
+        this.password = await bcrypt.hash(this.password, 8)
+    }
 })
 
 const UserRm = model('UserRm' , userSchema)
