d37eef8b374a5676363b07ad112256232ab4e651
Juan <jd.berbeo@gmail.com>
2021-06-22 16:43:00 -0500

    se dejan rutas de post signup y post sign in

--- src/routes/inspector.js
+++ src/routes/inspector.js
@@ -3,10 +3,13 @@ const router = require('express').Router()
 
 router.route('/signup').post((req, res) => { res.send('ok sign post '); })
 router.route('/login').post((req, res) => { res.send('ok login post'); })
-router.route('/logout').post((req, res) => { res.send('ok post logout'); })
-router.route('/inspector').get((req, res) => { res.send('ok get inspector '); })
-router.route('/editProfile').put((req, res) => { res.send('ok editprofile '); })
-router.route('/profile').delete((req, res) => { res.send('ok Delete profile '); })
+
+//borrar token
+// router.route('/logout').post((req, res) => { res.send('ok post logout'); }) 
+//las demás se realizan según necesidad del proyecto 
+// router.route('/inspector').get((req, res) => { res.send('ok get inspector '); })
+// router.route('/editProfile').put((req, res) => { res.send('ok editprofile '); })
+// router.route('/profile').delete((req, res) => { res.send('ok Delete profile '); })
 
 module.exports = router
 
