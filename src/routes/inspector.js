7e80d9195b2d61796aff18e5581b9b870b481f9a
Juan <jd.berbeo@gmail.com>
2021-06-21 22:46:23 -0500

    crea rutas del inspector signup,login,logout,inspector,editProfile,profile

---
+++ src/routes/inspector.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+// const inspectorController = require('../controllers')
+
+router.route('/signup').post((req, res) => { res.send('ok sign post '); })
+router.route('/login').post((req, res) => { res.send('ok login post'); })
+router.route('/logout').post((req, res) => { res.send('ok post logout'); })
+router.route('/inspector').get((req, res) => { res.send('ok get inspector '); })
+router.route('/editProfile').put((req, res) => { res.send('ok editprofile '); })
+router.route('/profile').delete((req, res) => { res.send('ok Delete profile '); })
+
+module.exports = router
+
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const connect = require("./db");
+const inspectorRouter = require('./routes/inspector')
 
 const app = express();
 connect();
@@ -10,6 +11,8 @@ connect();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+
+app.use('/inspectors', inspectorRouter)
 
 app.listen(8000, () => {
   console.log("app running at http://localhost:8000");
