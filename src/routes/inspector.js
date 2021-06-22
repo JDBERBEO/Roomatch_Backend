792c961c445ecefb855f82eb7007496fdbfcbf8b
Juan <jd.berbeo@gmail.com>
2021-06-22 16:44:41 -0500

    rutas definitivas de signup y sign in

--- src/routes/inspector.js
+++ src/routes/inspector.js
@@ -1,15 +1,8 @@
 const router = require('express').Router()
-// const inspectorController = require('../controllers')
+const inspectorController = require('../controllers')
 
 router.route('/signup').post((req, res) => { res.send('ok sign post '); })
 router.route('/login').post((req, res) => { res.send('ok login post'); })
-
-//borrar token
-// router.route('/logout').post((req, res) => { res.send('ok post logout'); }) 
-//las demás se realizan según necesidad del proyecto 
-// router.route('/inspector').get((req, res) => { res.send('ok get inspector '); })
-// router.route('/editProfile').put((req, res) => { res.send('ok editprofile '); })
-// router.route('/profile').delete((req, res) => { res.send('ok Delete profile '); })
 
 module.exports = router
 
