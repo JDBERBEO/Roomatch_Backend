05f6f9418927e894151e6b04e0cb849c42c00ad7
LuisAaronHernandezRangel <lahr.4444@gmail.com>
2021-06-22 17:34:10 -0500

    APT26_Crear_controlador_signIn_en_Usercontroller_con_signUp

--- src/controllers/userRm.controller.js
+++ src/controllers/userRm.controller.js
@@ -17,6 +17,42 @@ module.exports = {
         }catch(err) {   
             res.status(400).json({ message: err.message })
          }
-      }
+      },
+   async signin(req, res) {
+     try {
+       const { email, password } = req.body
+ 
+       const user = await User.findOne({ email })
+ 
+       if(!user) {
+         throw new Error('Password or invalid email')
+       }
+ 
+       const isValid = await bcrypt.compare(password, user.password)
+ 
+       if(!isValid) {
+         throw new Error('Password or invalid email')
+       }
+ 
+       const token = jwt.sign(
+         { userId: user._id },
+         process.env.SECRET,
+         { expiresIn: 60 * 60 * 24 * 365 }
+       )
+ 
+       res.status(201).json({ token })
+     } catch(error) {
+       res.status(400).json({ message: error.message })
+     }
+   },
+   async show(req, res) {
+     try {
+       const { user } = req
+       const profile = await User.findById(user).select('-password')
+       res.status(200).json(profile)
+     } catch(error) {
+       res.status(404).json({ message: error.message })
+     }
    }
+ }
 
