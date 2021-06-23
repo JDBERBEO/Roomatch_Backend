eeeabe5e2b6dbe1854ace6dba22c911047653022
Juan Sebastian Quinonez Murillo <juansebastianquinonezmurillo@Mac-mini-de-Juan.local>
2021-06-22 21:54:56 -0500

    space advertisement route

---
+++ src/routes/spaAdver.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const adverController = require("../controllers/spaAdverController.js");
+
+router.route("/").post(adverController.create);
+router.route("/").get(adverController.list);
+router.route("/:userId").get(adverController.show);
+router.route("/:userId").put(adverController.update);
+router.route("/:userId").delete(adverController.destroy);
+
+module.exports = router;
