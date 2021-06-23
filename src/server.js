b9b87585189a7c426e7b641b778c556318bf0eee
kellycora <kellyc@evollve.co>
2021-06-22 21:46:50 -0500

    se crea rutas de la reservacion y se actuliza server.js con app use para reservation

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 const connect = require("./db");
 const roomieRouter = ('./routes/roomie.js')
+const reservationRouter = require('./routes/reservation')
 
 const app = express();
 connect();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(morgan("dev"));
 
 app.use('/roomie', roomieRouter)
+app.use ('/reservations', reservationRouter)
 
 app.listen(8000, () => {
   console.log("app running at http://localhost:8000");
