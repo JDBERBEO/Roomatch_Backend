38957f2b5e86073aec9a6ab79425fcf667b06f3c
jsqmicg <80492686+jsqmicg@users.noreply.github.com>
2021-06-28 07:37:09 -0500

    Correciones spadvertisement (#20)
    * conflictos_correcciones
    
    * cambios rutas spaadver
    
    Co-authored-by: Juan Sebastian Quinonez Murillo <juansebastianquinonezmurillo@Mac-mini-de-Juan.local>

--- src/controllers/spaAdverController.js
+++ src/controllers/spaAdverController.js
@@ -2,7 +2,6 @@ const Advertisement = require("../models/SpaAdverModel");
 const Host = require("../models/UserHostModel");
 
 module.exports = {
-<<<<<<< HEAD
   async create(req, res) {
     try {
       const {
@@ -12,42 +11,6 @@ module.exports = {
       const advertisement = await Advertisement.create({
         ...body,
         host: hostId,
-=======
-  create(req, res) {
-    const {
-      body,
-      params: { hostId },
-    } = req;
-    Advertisement.create({
-      ...body,
-      host: hostId,
-    })
-<<<<<<< HEAD
-      .then((advertisement) => {
-        Host.findById(hostId).then((host) => {
-          host.posts.push(advertisement._id);
-
-          host
-            .save({ validateBeforeSave: false })
-            .then(() => {
-              res.status(201).json(advertisement);
-            })
-            .catch((err) => {
-              res.status(400).json({ message: err.message });
-            });
-        });
-=======
-      .then((adver) => {
-        res.status(201).json(adver);
->>>>>>> develop
-      })
-      .catch((err) => {
-<<<<<<< HEAD
-        res.status(400).json({ message: "something went wrong" });
->>>>>>> develop
-=======
-        res.status(400).json({ message: err.message });
->>>>>>> develop
       });
       const host = await Host.findById(hostId);
       host.posts.push(advertisement._id);
@@ -58,7 +21,6 @@ module.exports = {
     }
   },
 
-<<<<<<< HEAD
   async list(req, res) {
     try {
       const { hostId } = req.params;
@@ -67,36 +29,8 @@ module.exports = {
     } catch (err) {
       res.status(400).json({ message: err.message });
     }
-=======
-  list(req, res) {
-    const { hostId } = req.params;
-    Advertisement.find({ host: hostId })
-      .then((adver) => {
-        res.status(201).json(adver);
-      })
-      .catch((err) => {
-<<<<<<< HEAD
-        res.status(400).json({ message: err.message });
-=======
-        res.status(400).json({ message: `something went wrong: ${err}` });
->>>>>>> develop
-      });
   },
 
-  show(req, res) {
-    const { adverId } = req.params;
-    Advertisement.findById(adverId)
-      .populate("host")
-      .then((adver) => {
-        res.status(200).json(adver);
-      })
-      .catch((err) => {
-        res.status(400).json({ message: err.message });
-      });
->>>>>>> develop
-  },
-
-<<<<<<< HEAD
   async show(req, res) {
     try {
       const { adverId } = req.params;
@@ -115,20 +49,6 @@ module.exports = {
       } = req;
       const adver = await Advertisement.findByIdAndUpdate(adverId, body, {
         new: true,
-=======
-  update(req, res) {
-    const {
-      params: { adverId },
-      body,
-    } = req;
-
-    Advertisement.findByIdAndUpdate(adverId, body, { new: true })
-      .then((adver) => {
-        res.status(200).json(adver);
-      })
-      .catch((err) => {
-        res.status(400).json({ message: err.message });
->>>>>>> develop
       });
       res.status(200).json(adver);
     } catch (err) {
@@ -136,7 +56,6 @@ module.exports = {
     }
   },
 
-<<<<<<< HEAD
   async destroy(req, res) {
     try {
       const { adverId } = req.params;
@@ -145,16 +64,5 @@ module.exports = {
     } catch (err) {
       res.status(400).json({ message: err.message });
     }
-=======
-  destroy(req, res) {
-    const { adverId } = req.params;
-    Advertisement.findByIdAndDelete(adverId)
-      .then((adver) => {
-        res.status(400).json(adver);
-      })
-      .catch((err) => {
-        res.status(400).json({ message: err.message });
-      });
->>>>>>> develop
   },
 };
