666e1742dcd8c5bfecfcb7ce4c6705a306bed14d
jsqmicg <80492686+jsqmicg@users.noreply.github.com>
2021-06-29 16:37:36 -0500

    Json webtoken host (#23)
    * se agrega encriptación a password host
    
    * se crea webtoken para el signup del host y para el sign in de este
    
    * autenticación host
    
    Co-authored-by: Juan Sebastian Quinonez Murillo <juansebastianquinonezmurillo@Mac-mini-de-Juan.local>

--- src/routes/userHost.js
+++ src/routes/userHost.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const userHostController = require("../controllers/userHostController");
+const { auth } = require("../utilis/middlewares");
 
 router.route("/").get(userHostController.list);
-router.route("/").post(userHostController.create);
+router.route("/signin").post(userHostController.signin);
+router.route("/signup").post(userHostController.signup);
+router.route("/profile").get(auth, userHostController.show);
 router.route("/:userhId").get(userHostController.show);
 router.route("/:userhId").put(userHostController.update);
 router.route("/:userhId").delete(userHostController.destroy);
