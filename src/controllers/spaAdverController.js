0a427274d67acf06c71e9b6fbbde271f3157c2df
Juan <jd.berbeo@gmail.com>
2021-07-24 22:52:11 -0500

    borrar console logs

--- src/controllers/spaAdverController.js
+++ src/controllers/spaAdverController.js
@@ -68,10 +68,8 @@ module.exports = {
 
   async showAll(req, res) {
     const { selectedDays, city } = req.query;
-    console.log("city", city);
-    console.log("selectedDays", selectedDays);
+
     const days = JSON.parse(selectedDays);
-    console.log("days", days);
 
     let filters = {};
     let Iso8001Days = [];
@@ -80,15 +78,11 @@ module.exports = {
       filters = {};
     } else {
       Iso8001Days = days.map((day) => new Date(day));
-      console.log("Iso8001 desde el else", Iso8001Days);
     }
 
-    console.log("Iso8001Days", Iso8001Days);
     const Iso8001DaysString = Iso8001Days.map((day) => day.toISOString());
-    console.log("Iso8001DaysString", Iso8001DaysString);
 
     try {
-      console.log("filters", filters);
       if (Iso8001DaysString) {
         filters.selectedDays = { $in: Iso8001DaysString };
       }
