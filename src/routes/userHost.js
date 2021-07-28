e9caa45e62a74fe28df2e00cb3d6c449ad92b9a3
Juan <jd.berbeo@gmail.com>
2021-07-27 22:20:26 -0500

    modifica userHost rutas

--- src/routes/userHost.js
+++ src/routes/userHost.js
@@ -3,11 +3,11 @@ const userHostController = require("../controllers/userHostController");
 const { auth } = require("../utils/middlewares");
 const { formData } = require("../utils/formData");
 
-//router.route("/").get(userHostController.list);
+router.route("/").get(userHostController.list);
 router.route("/signin").post(userHostController.signin);
 router.route("/signup").post(userHostController.signup);
 router.route("/profile").get(auth, userHostController.show);
-//router.route("/:userhId").get(userHostController.show);
+router.route("/:userhId").get(userHostController.show);
 router.route("/profile").put(auth, formData, userHostController.update);
 router.route("/:userhId").delete(userHostController.destroy);
 
